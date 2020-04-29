28a4607e565604377d9b93fce8eb46fd6e4fa754
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-29 14:48:22 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,7 +23,7 @@ def literal_aoh
   # held in the constants
 
 
-  Array.new (DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G)
+  Array.new = DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G
 end
 
 def aoh_lookup(aoh, row, key)
